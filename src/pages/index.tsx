36c9c31781768390d77c6d2ee2e6b4475173c3c1
dialubern <defantedealovern@gmail.com>
2023-04-20 16:18:20 +0800

    Add subject page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,9 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signin from './Signin';
 import Dashboard from './Dashboard';
 import Students from './Students';
+import AddSubject from './AddSubject';
 import EditAdmin from './EditAdmin';
+
 
 export default function Pages() {
   return (
@@ -12,6 +14,7 @@ export default function Pages() {
         <Route element={<Dashboard />} path='/dashboard' />
         <Route element={<Students />} path='/students' />
         <Route element={<EditAdmin />} path='/editadmin' />
+        <Route element={<AddSubject />} path='/addsubject' />
       </Routes>
     </BrowserRouter>
   )

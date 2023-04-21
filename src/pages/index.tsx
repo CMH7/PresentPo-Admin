f2cd3d275f8c2051912a1be7210bb8a66f2a9e12
dialubern <defantedealovern@gmail.com>
2023-04-21 21:09:03 +0800

    editschedule page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Signin from './Signin';
 import Dashboard from './Dashboard';
 import Students from './Students';
+import EditSchedule from './EditSchedule';
 
 export default function Pages() {
   return (
@@ -10,6 +11,7 @@ export default function Pages() {
         <Route element={<Signin />} path='/' />
         <Route element={<Dashboard />} path='/dashboard' />
         <Route element={<Students />} path='/students' />
+        <Route element={<EditSchedule />} path='/editschedule' />
       </Routes>
     </BrowserRouter>
   )

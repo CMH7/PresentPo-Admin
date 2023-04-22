12bbae63e114777030c94d90f2ba43864d6d130b
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-04-22 10:00:57 +0800

    Dashboard testing

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,51 +1,21 @@
 import { useEffect } from "react";
 import Wrapper from "../components/Wrapper";
-import { useNavigate } from "react-router-dom";
-import { gql, useLazyQuery } from "@apollo/client";
-import QueryResult from "../components/QueryResult";
+import { useSelector } from 'react-redux'
+import { selectAdmin } from "../selectors";
 
-const CHECK_ADMIN_CREDENTIALS = gql`
-  query CheckAdminCredentials($email: String!, $password: String!) {
-    checkAdminCredentials(email: $email, password: $password)
-  }
-`
 
 export default function Dashboard() {
-  const navigate = useNavigate()
-  const [ checkAdminCredentials, { loading, data} ] = useLazyQuery(CHECK_ADMIN_CREDENTIALS)
-
-  const checkCreds = () => {
-    checkAdminCredentials({ variables: { email: localStorage.getItem('email'), password: localStorage.getItem('pass') } }).then(res => {
-      if (!res?.data?.checkAdminCredentials) {
-        navigate('/', {
-          replace: true
-        })
-      }
-    }).catch(err => {
-      navigate('/', {
-        replace: true
-      })
-    })
-    
-  }
+  const admin = useSelector(selectAdmin)
 
   useEffect(() => {
     document.title = 'Dashboard';
-
-    if (localStorage.getItem('email') != null && localStorage.getItem('pass') != null) {
-      checkCreds()
-    } else {
-      navigate('/', {
-        replace: true
-      })
-    }
   }, [])
 
   return (
     <Wrapper centered={true}>
-      <QueryResult loading={loading} data={data?.checkAdminCredentials} error={null}>
-        <h1>Dashboard</h1>
-      </QueryResult>
+      <div>
+        {admin.name.first} {admin.name.middle} {admin.name.last} {admin.name.extension}
+      </div>
     </Wrapper>
   )
 }

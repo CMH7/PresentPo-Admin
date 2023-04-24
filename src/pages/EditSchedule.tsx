ed10cb7474ee03048c6b2153850f8736e4ac6679
roninyalynn <roninyaragudo52@gmail.com>
2023-04-24 01:26:57 -0700

    changed <div classname> to <label classname>

--- src/pages/EditSchedule.tsx
+++ src/pages/EditSchedule.tsx
@@ -8,20 +8,20 @@ export default function EditSchedule() {
 			{/* top */}
 			<div className="flex justify-between">
 
-			{/* back button  */}
-			<div className="flex items-center mt-[50px] ml-[100px]">
-				<img className="w-[25px] h-[25px] mr-[30px]" src={backIcon} alt="back icon"
-				/>
-				{/* add button  */}
-				<div className="poppins font-bold text-[40px] text-primary-2">
-					Edit Schedule
+				{/* back button  */}
+				<div className="flex items-center mt-[50px] ml-[100px]">
+					<img className="w-[25px] h-[25px] mr-[30px]" src={backIcon} alt="back icon" />
+					
+					{/* edit label  */}
+					<label className="poppins font-bold text-[40px] text-primary-2">
+						Edit Schedule
+					</label>
 				</div>
-			</div>
 
-			{/* save button */}
-			<button className="flex justify-center items-center mt-[50px] mr-[100px] bg-[#11CF00] hover:bg-[#1672ec] text-white font-semibold py-2 px-20 rounded-[50px] focus:outline-none focus:shadow-outline w-[218px] h-[55px]" type="submit">
-				Save
-			</button>
+				{/* save button */}
+				<button className="flex justify-center items-center mt-[50px] mr-[100px] bg-[#11CF00] hover:bg-[#1672ec] text-white font-semibold py-2 px-20 rounded-[50px] focus:outline-none focus:shadow-outline w-[218px] h-[55px]" type="submit">
+					Save
+				</button>
 			</div>
 
 			{/* parent - add 2 columns */}
@@ -52,9 +52,9 @@ export default function EditSchedule() {
 						{/* dropdown for day  */}
 						<div className="flex w-[227px] flex-wrap border-2 border-transparent rounded-[10px] relative pt-[15px]">
 							<div className="w-full h-[19px] flex justify-center items-center absolute left-0 -top-3">
-								<div className="w-[81px] h-full bg-primary-2 m-8 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
+								<label className="w-[81px] h-full bg-primary-2 m-8 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
 									Day
-								</div>
+								</label>
 							</div>
 
 							<div className="flex w-full gap-x-[10px]">
@@ -74,9 +74,9 @@ export default function EditSchedule() {
 						{/* start time  */}
 						<div className="flex w-[250px] flex-wrap border-2 border-primary-2 rounded-[10px] p-2 relative pt-[15px]">
 							<div className="w-full h-[19px] flex justify-center items-center absolute left-0 -top-3">
-								<div className="w-[81px] h-full bg-primary-2 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
+								<label className="w-[81px] h-full bg-primary-2 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
 									Start time
-								</div>
+								</label>
 							</div>
 
 							{/* hour and minute  */}
@@ -176,9 +176,9 @@ export default function EditSchedule() {
 						{/* end time  */}
 						<div className="flex w-[250px] flex-wrap border-2 border-primary-2 rounded-[10px] p-2 relative pt-[15px]">
 							<div className="w-full h-[19px] flex justify-center items-center absolute left-0 -top-3">
-								<div className="w-[81px] h-full bg-primary-2 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
+								<label className="w-[81px] h-full bg-primary-2 text-white poppins font-semibold text-[14px] flex justify-center items-center rounded-[3px]">
 									End time
-								</div>
+								</label>
 							</div>
 
 							{/* hour and minute  */}
@@ -283,60 +283,56 @@ export default function EditSchedule() {
 						<label className="block text-white poppins font-semibold pb-[10px]">
 							Assigned Faculty
 						</label>
-
-							<select className="peer h-[48px] w-[558px] rounded-[10px] border border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
-								<option value="faculty1"></option>
-								<option value="faculty2"></option>
-								<option value="faculty3"></option>
-							</select>
+						<select className="peer h-[48px] w-[558px] rounded-[10px] border border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
+							<option value="faculty1"></option>
+							<option value="faculty2"></option>
+							<option value="faculty3"></option>
+						</select>
 					</div>
 
 					{/* dropdown for strand  */}
 					<div className="flex w-[138px] gap-x-[30px] poppins">
 						<div className="flex w-[227px] flex-wrap rounded-[10px] relative pt-[15px]">
-							<div className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold pb-[10px]">
+							<label className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold pb-[10px]">
 								Strand
-							</div>
-
+							</label>
 							<div className="flex w-full gap-x-[10px]">
 								<select className="peer h-[48px] w-[132px] rounded-[10px] border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
-								<option value="abm">ABM</option>
-										<option value="gas">GAS</option>
-										<option value="humss">HUMSS</option>
-										<option value="stem">STEM</option>
+									<option value="abm">ABM</option>
+									<option value="gas">GAS</option>
+									<option value="humss">HUMSS</option>
+									<option value="stem">STEM</option>
 								</select>
 							</div>
+						</div>
+
+						{/* dropdown for grade level  */}
+						<div className="flex flex-wrap rounded-[10px] relative pt-[15px]">
+							<label className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold">
+								Grade Level
+							</label>
+							<div className="flex w-full gap-x-[10px]">
+								<select className="peer h-[48px] w-[137px] rounded-[10px] border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border 	placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
+									<option value="grade11">11</option>
+									<option value="grade12">12</option>
+								</select>
+							</div>
+						</div>
+
+						{/* dropdown for Section */}
+						<div className="flex w-[227px] flex-wrap  rounded-[10px] relative pt-[15px]">
+							<label className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold">
+								Section
+							</label>
+							<div className="flex w-full gap-x-[10px]">
+								<select className="peer h-[48px] w-[235px] rounded-[10px] border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
+									<option value="section1"></option>
+									<option value="section2"></option>
+									<option value="section3"></option>
+								</select>
+							</div>
+						</div>
 					</div>
-
-					{/* dropdown for grade level  */}
-					<div className="flex flex-wrap rounded-[10px] relative pt-[15px]">
-						<div className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold">
-							Grade Level
-						</div>
-
-						<div className="flex w-full gap-x-[10px]">
-							<select className="peer h-[48px] w-[137px] rounded-[10px] border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border 	placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
-								<option value="grade11">11</option>
-								<option value="grade12">12</option>
-							</select>
-						</div>
-					</div>
-
-					{/* dropdown for Section */}
-					<div className="flex w-[227px] flex-wrap  rounded-[10px] relative pt-[15px]">
-						<div className="flex justify-center items-center absolute left-0 -top-3 poppins text-white font-semibold">
-							Section
-						</div>
-
-						<div className="flex w-full gap-x-[10px]">
-							<select className="peer h-[48px] w-[235px] rounded-[10px] border-blue-gray-200 bg-white poppins text-[16px] font-semibold text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 empty:!bg-red-500 disabled:border-0 disabled:bg-blue-gray-50">
-								<option value="section1"></option>
-								<option value="section2"></option>
-								<option value="section3"></option>
-							</select>
-						</div>
-					</div>
-				</div>
 				</div>
 			</div>
 		</Wrapper>

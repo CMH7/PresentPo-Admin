40186c51f781337a975aa8275fe107856ec642c0
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-05-28 15:39:13 +0800

    new UI design for refresh and as of label

--- src/components/Wrapper.tsx
+++ src/components/Wrapper.tsx
@@ -1,5 +1,7 @@
 import { ReactNode } from 'react';
 import refreshIcon from '../assets/refresh.png'
+import { format } from 'date-fns';
+import { utcToZonedTime } from 'date-fns-tz';
 
 interface WrapperProps {
   children: ReactNode;
@@ -10,7 +12,14 @@ interface WrapperProps {
 }
 
 export default function Wrapper({ children, centered = false, centerX = false, centerY = false, klass = '' }: WrapperProps) {
-  // 'h-[100dvh] bg-gradient-to-br from-primary-0 to-primary-1 mont'
+  const currentLocation = location.href.split('/')[location.href.split('/').length - 1]
+  
+  const datee = new Date();
+  const timeZone = 'Asia/Singapore';
+
+  const zonedDate = utcToZonedTime(datee, timeZone);
+  
+  const formattedDate = format(zonedDate, "EEE, dd MMM yyyy hh:mm aa");
 
   let stylee = centered || (centerX && centerY) ? ' relative h-[100dvh] bg-primary-2 mont flex flex-col justify-center items-center' : centerX ? ' relative h-[100dvh] bg-primary-2 mont flex flex-col items-center' : centerY ? ' relative h-[100dvh] bg-primary-2 flex flex-col justify-center mont' : ' relative h-[100dvh] bg-primary-2 mont'
   
@@ -19,16 +28,29 @@ export default function Wrapper({ children, centered = false, centerX = false, c
   return (
     <div className={stylee}>
       {children}
-      <div onClick={() => {
-        location.reload()
-      }} className=' cursor-pointer group absolute w-[60px] hover:w-[200px] transition-all h-[60px] hover:h-fit overflow-clip bottom-[50px] right-[50px] bg-white p-5 flex items-center justify-center gap-x-[10px] rounded-full '>
-        <div className=' hidden group-hover:block font-bold poppins text-[18px] select-none '>
-          Refresh
-        </div>
+
+      <div
+        onClick={() => {
+          location.reload()
+        }}
+        className={` select-none cursor-pointer group text-white text-[15px] poppins rounded-tl-2xl z-50 absolute bottom-0 right-0 pr-[20px] py-2 pl-4 flex items-center justify-center ${currentLocation === 'admindashboard' ? 'bg-primary-2' : ''} `}>
+        {/* as of  */}
         <div>
-          <img src={refreshIcon} alt="Refresh Icon from FlatIcon" />
+          Latest data as of {formattedDate}
+        </div>
+
+        {/* refresh button  */}
+        <div className=' w-[50px] group-hover:w-auto bg-transparent group-hover:rounded-full group-hover:bg-white ml-0 group-hover:ml-2 transition-all h-fit overflow-clip flex items-center justify-center '>
+          <div className=' group-hover:text-primary-2 border-l border-l-white pl-[10px] w-0 mx-0 group-hover:mx-2 group-hover:w-[65px] overflow-clip transition-all '>
+            Refresh
+          </div>
+          <div>
+            <img className='invert group-hover:filter-none' src={refreshIcon} alt="Refresh Icon from FlatIcon" />
+          </div>
         </div>
       </div>
+
+
     </div>
   )
 }

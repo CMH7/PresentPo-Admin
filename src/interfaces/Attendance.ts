60d6a62616879a1dcb946c329ff4cf05e94a11f0
Charles Maverick Herrera <90278189+CMH7@users.noreply.github.com>
2023-05-28 14:18:08 +0800

    Fixed interface Attendance

--- src/interfaces/Attendance.ts
+++ src/interfaces/Attendance.ts
@@ -1,18 +1,12 @@
+import Datee from "./Date"
+
 interface Attendance {
   id: string
-  date: attDate
+  date: Datee
   qr: string
   schedule: string
   special: boolean
   label: string
-}
-
-interface attDate {
-  minute: number
-  hour: number
-  day: number
-  month: number
-  year: number
 }
 
 export default Attendance
